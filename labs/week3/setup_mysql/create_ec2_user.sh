7a5321c181332874b2669ffb921db5b03321f798
Abdul Khan <abdulk@gmail.com>
2016-03-13 20:36:24 -0400

    Repo Commit at COMMIT_03-13-16-20:36:24

--- labs/week3/setup_mysql/create_ec2_user.sh
+++ labs/week3/setup_mysql/create_ec2_user.sh
@@ -5,6 +5,9 @@ mysql <<EOF
 
 CREATE USER 'ec2'@'localhost' IDENTIFIED BY 'cloudera';
 CREATE USER 'ec2'@'%' IDENTIFIED BY 'cloudera';
+grant all on retail_db.* TO 'ec2'@'%' IDENTIFIED BY 'cloudera';
+grant all on airlines.* TO 'ec2'@'%' IDENTIFIED BY 'cloudera';
+grant all on employees.* TO 'ec2'@'%' IDENTIFIED BY 'cloudera';
 
 GRANT ALL PRIVILEGES ON * . * TO 'ec2'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'ec2'@'%';
